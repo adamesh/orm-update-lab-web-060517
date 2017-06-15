0841035181f8a3bd055a3a05338bb7229ce2aca5
adamesh <adameshh@gmail.com>
2017-06-15 13:18:01 -0400

    got 3 passing

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,28 @@
 require_relative "../config/environment.rb"
+require 'pry'
 
 class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  attr_accessor :name, :grade
+  attr_reader :id
 
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+    @id = nil
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      );
+    SQL
+    DB[:conn].execute(sql)
+  end
 
 end
